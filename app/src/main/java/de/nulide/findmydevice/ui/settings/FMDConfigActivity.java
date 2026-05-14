5c1b1666e92878e92eba624acce9433d1d62fc16
Thore Goebel <git@thore.io>
2026-05-14 14:38:41 +0200

    Remove FMD PIN section from General settings

--- app/src/main/java/de/nulide/findmydevice/ui/settings/FMDConfigActivity.java
+++ app/src/main/java/de/nulide/findmydevice/ui/settings/FMDConfigActivity.java
@@ -1,14 +1,10 @@
 package de.nulide.findmydevice.ui.settings;
 
-import static de.nulide.findmydevice.commands.CommandHandlerKt.availableCommands;
 import static de.nulide.findmydevice.ui.UiUtil.setupEdgeToEdgeAppBar;
 import static de.nulide.findmydevice.ui.UiUtil.setupEdgeToEdgeScrollView;
-import static de.nulide.findmydevice.utils.CypherUtils.MIN_PASSWORD_LENGTH;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -22,8 +18,6 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import de.nulide.findmydevice.R;
 import de.nulide.findmydevice.data.EncryptedSettingsRepository;
@@ -39,8 +33,6 @@ public class FMDConfigActivity extends FmdActivity implements CompoundButton.OnC
     private EncryptedSettingsRepository encSettings;
 
     private CheckBox checkBoxDeviceWipe;
-    private CheckBox checkBoxAccessViaPin;
-    private Button buttonEnterPin;
     private Button buttonSelectRingtone;
     private Button buttonDeletePassword;
     private EditText editTextLockScreenMessage;
@@ -68,10 +60,6 @@ public class FMDConfigActivity extends FmdActivity implements CompoundButton.OnC
         checkBoxDeviceWipe.setChecked((Boolean) settings.get(Settings.SET_WIPE_ENABLED));
         checkBoxDeviceWipe.setOnCheckedChangeListener(this);
 
-        checkBoxAccessViaPin = findViewById(R.id.checkBoxFMDviaPin);
-        checkBoxAccessViaPin.setChecked((Boolean) settings.get(Settings.SET_ACCESS_VIA_PIN));
-        checkBoxAccessViaPin.setOnCheckedChangeListener(this);
-
         editTextLockScreenMessage = findViewById(R.id.editTextTextLockScreenMessage);
         editTextLockScreenMessage.setText((String) settings.get(Settings.SET_LOCKSCREEN_MESSAGE));
         editTextLockScreenMessage.addTextChangedListener(this);
@@ -80,10 +68,6 @@ public class FMDConfigActivity extends FmdActivity implements CompoundButton.OnC
         colorDisabled = getColor(R.color.md_theme_error);
         textColorEnabled = getColor(R.color.md_theme_onPrimary);
         textColorDisabled = getColor(R.color.md_theme_onError);
-
-        buttonEnterPin = findViewById(R.id.buttonEnterPin);
-        buttonEnterPin.setOnClickListener(this::onEnterPinClicked);
-        updatePinButton();
 
         buttonSelectRingtone = findViewById(R.id.buttonSelectRingTone);
         buttonSelectRingtone.setOnClickListener(this::onSelectRingtoneClicked);
@@ -102,9 +86,6 @@ public class FMDConfigActivity extends FmdActivity implements CompoundButton.OnC
         if (buttonView == checkBoxDeviceWipe) {
             settings.set(Settings.SET_WIPE_ENABLED, isChecked);
             updateDeletePasswordButton();
-        } else if (buttonView == checkBoxAccessViaPin) {
-            settings.set(Settings.SET_ACCESS_VIA_PIN, isChecked);
-            updatePinButton();
         }
     }
 
@@ -130,41 +111,6 @@ public class FMDConfigActivity extends FmdActivity implements CompoundButton.OnC
                 settings.set(Settings.SET_FMD_COMMAND, edited.toString().toLowerCase());
             }
         }
-    }
-
-    private void onEnterPinClicked(View v) {
-        Context context = v.getContext();
-        View pinLayout = getLayoutInflater().inflate(R.layout.dialog_pin, null);
-
-        EditText editTextPin = pinLayout.findViewById(R.id.editTextPin);
-
-        new MaterialAlertDialogBuilder(context)
-                .setTitle(getString(R.string.Settings_Enter_Pin))
-                .setView(pinLayout)
-                .setPositiveButton(getString(R.string.Ok), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String pin = editTextPin.getText().toString();
-
-                        if (pin.isBlank()) {
-                            encSettings.setFmdPin(null);
-                        }
-                        // The PIN must not match a command keyword.
-                        // Otherwise, we cannot (easily) distinguish between the PIN and the command.
-                        // Also, it would be a weak PIN anyway.
-                        else if (
-                                availableCommands(context).stream().anyMatch(cmd -> cmd.getKeyword().equals(pin))
-                        ) {
-                            Toast.makeText(context, R.string.pin_match_command_keyword, Toast.LENGTH_LONG).show();
-                        } else if (pin.length() < MIN_PASSWORD_LENGTH) {
-                            Toast.makeText(context, R.string.pin_min_length, Toast.LENGTH_LONG).show();
-                        } else {
-                            encSettings.setFmdPin(pin);
-                        }
-
-                        updatePinButton();
-                    }
-                })
-                .show();
     }
 
     private void onEnterDeletePasswordClicked(View v) {
@@ -193,19 +139,6 @@ public class FMDConfigActivity extends FmdActivity implements CompoundButton.OnC
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_RINGTONE) {
             Uri uri = intent.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
             settings.set(Settings.SET_RINGER_TONE, uri.toString());
-        }
-    }
-
-    private void updatePinButton() {
-        String pin = encSettings.getFmdPin();
-        if (pin == null || pin.isBlank()) {
-            buttonEnterPin.setBackgroundColor(colorDisabled);
-            buttonEnterPin.setTextColor(textColorDisabled);
-            buttonEnterPin.setText(R.string.Settings_Set_Pin);
-        } else {
-            buttonEnterPin.setBackgroundColor(colorEnabled);
-            buttonEnterPin.setTextColor(textColorEnabled);
-            buttonEnterPin.setText(R.string.Settings_Change_Pin);
         }
     }
 
